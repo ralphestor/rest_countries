5f2d6b57b230f6cb096afad1f5e0c92965d751bd
Ralph <ralphestor30@gmail.com>
2022-02-13 08:54:45 +0800

    removed useref

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import CountryCard from './components/CountryCard';
 import axios from 'axios';
 import './Home.css';
